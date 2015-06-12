d6241b0217ea05cedb63c44e49e0d69d32886938
Babajide Fowotade <babajide.fowotade@andela.co>
2015-06-12 16:23:18 +0100

    comments

--- index.py
+++ index.py
@@ -7,10 +7,19 @@ def hello_world():
     return 'Hello Andela!'
 
 # An individual link
+@app.route('/comment')
+def comment():
+  return 'Comments'
 
 # Posting a link
+@app.route('/posts')
+def post():
+  return 'Post things here'
 
 # Commenting on a link
+@app.route('/comment/<int:comment_id>')
+def comment_link(comment_id):
+  return 'Comments on a link'+ str(comment_id)
 
 if __name__ == '__main__':
     app.run(debug = True)
