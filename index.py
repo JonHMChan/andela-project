8f8d313701ed21a530b9ffe4caa5b7119ac9798d
Jon Chan <jchan@stackoverflow.com>
2015-06-12 11:10:41 -0400

    Basic structure of application

--- index.py
+++ index.py
@@ -1,9 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Home Page
 @app.route('/')
 def hello_world():
     return 'Hello Andela!'
+
+# An individual link
+
+# Posting a link
+
+# Commenting on a link
 
 if __name__ == '__main__':
     app.run(debug = True)
