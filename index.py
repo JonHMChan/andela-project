76929bf449a2224ade18a3346e70a1b6b9578cdf
Jon Chan <jchan@stackoverflow.com>
2015-06-12 11:09:45 -0400

    Add basic flask application

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-print "Hello"
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello Andela!'
+
+if __name__ == '__main__':
+    app.run(debug = True)
