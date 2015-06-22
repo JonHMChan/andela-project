88813261710210f015649e37d95e1b2b82e558fe
Jon Chan <jchan@stackoverflow.com>
2015-06-22 11:36:37 -0400

    Remove incomplete function

--- index.py
+++ index.py
@@ -112,11 +112,5 @@ def submitComment():
         comment = request.form['comment']
         return json.dumps({'status': 'OK', 'comment': comment})
 
-#linkedin
-@app.route('/auth/callback')
-def linkedInAuth():
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
