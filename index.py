422e50ba7f339166ed076a5078ed7375377d3b4b
Babajide Fowotade <babajide.fowotade@andela.co>
2015-06-22 17:31:53 +0100

    add host network ip to index.py

--- index.py
+++ index.py
@@ -130,4 +130,4 @@ def authLinkedinCallback():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
