f3c2ffe05e6037d9e0cf4841c092feb0517e9958
Babajide Fowotade <babajide.fowotade@andela.co>
2015-07-21 15:45:20 +0100

    alembic revisions

--- models.py
+++ models.py
@@ -3,8 +3,6 @@ from sqlalchemy.dialects.postgresql import JSON
 from datetime import datetime
 
 
-
-
 class User(db.Model):
     __tablename__ = 'users'
 
@@ -14,9 +12,10 @@ class User(db.Model):
     lastname = db.Column(db.String)
     email = db.Column(db.String(80), unique=True)
     registeredOn = db.Column(db.DateTime)
-    photo = db.Column(db.String)
+    photo = db.Column(db.String,
+                      default='https://photos-4.dropbox.com/t/2/AAD319Bvyn9kvro8Xr_bIrCJHATB4FkbHfIQphqf2-9ucw/12/361580150/jpeg/32x32/1/1437490800/0/2/gravatar-60-grey.jpg/CPaMtawBIAEgAiADIAQgBSAGIAcoAg/Iw51usTm6UShHbHahHF-ZcwIau5oCgZnuHwjdU4cPXs?size=1280x960&size_mode=2')
     job = db.Column(db.String)
-    major_skill = db.Column(db.Unicode(50))
+    major_skill = db.Column(db.String(100), default='Nothing')
     other_skills = db.Column(JSON)
     about = db.Column(db.String)
     had_known = db.Column(db.String)
@@ -25,6 +24,7 @@ class User(db.Model):
     social_linkedin = db.Column(db.String)
     social_twitter = db.Column(db.String)
     social_github = db.Column(db.String)
+    vip = db.Column(db.Boolean, default=False)
 
     def __init__(self, uid, firstname, lastname, email):
         self.uid = uid
@@ -67,6 +67,7 @@ class User(db.Model):
             social_github=self.social_github,
         )
 
+
 class MongoIndex(mongodb.Document):
     firstname = mongodb.StringField(required=True)
     skill = mongodb.StringField(required=True, max_length=100)
@@ -76,6 +77,6 @@ class MongoIndex(mongodb.Document):
     meta = {
         'allow_inheritance': True,
         'indexes': [{'fields': ['$firstname', '$skill', 'photo', 'email'],
-           'default_language': 'english'}],
+                     'default_language': 'english'}],
         'ordering': ['-firstname']
     }
