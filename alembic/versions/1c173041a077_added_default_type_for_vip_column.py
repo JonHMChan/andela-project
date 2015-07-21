541bd1e28d141d2b2acf87e2a2ef25f23b3aa8ad
Babajide Fowotade <babajide.fowotade@andela.co>
2015-07-21 15:54:40 +0100

    alembic integration

---
+++ alembic/versions/1c173041a077_added_default_type_for_vip_column.py
@@ -0,0 +1,24 @@
+"""added default type for vip column
+
+Revision ID: 1c173041a077
+Revises: 53c6499140b3
+Create Date: 2015-07-21 15:42:14.971422
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1c173041a077'
+down_revision = '53c6499140b3'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ alembic/versions/53c6499140b3_change_type_for_major_skill_column.py
@@ -0,0 +1,24 @@
+"""change type for major_skill column
+
+Revision ID: 53c6499140b3
+Revises: 30014e31acf
+Create Date: 2015-07-21 15:41:12.241469
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '53c6499140b3'
+down_revision = '30014e31acf'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
