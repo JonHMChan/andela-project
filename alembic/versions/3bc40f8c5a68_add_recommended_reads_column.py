f438e7e2ece139984bcc9daec334e599fc31619c
Babajide Fowotade <babajide.fowotade@andela.co>
2015-07-29 12:48:19 +0100

    added images file

---
+++ alembic/versions/3bc40f8c5a68_add_recommended_reads_column.py
@@ -0,0 +1,24 @@
+"""add recommended_reads column
+
+Revision ID: 3bc40f8c5a68
+Revises: 1c173041a077
+Create Date: 2015-07-24 00:29:37.986809
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3bc40f8c5a68'
+down_revision = '1c173041a077'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
