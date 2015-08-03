22ed839478999e2be0ca09db4876c3f78cba6e69
Babajide Fowotade <babajide.fowotade@andela.co>
2015-08-03 22:52:37 +0100

    added new alembic migration script

---
+++ alembic/versions/55c0f65febe6_change_vip_column.py
@@ -0,0 +1,24 @@
+"""change vip column
+
+Revision ID: 55c0f65febe6
+Revises: 4ad014956f76
+Create Date: 2015-08-03 22:42:00.787876
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '55c0f65febe6'
+down_revision = '4ad014956f76'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
