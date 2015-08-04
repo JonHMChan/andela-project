1bfe0055b28dfc8631cdf748c7fe0c58911173d2
Babajide Fowotade <babajide.fowotade@andela.co>
2015-08-04 15:02:53 +0100

    refactored code complexity

---
+++ vipmembers_config.py
@@ -0,0 +1,27 @@
+from flask import request, render_template, abort, jsonify
+from flask.ext.login import current_user
+from setup import app
+from models import *
+PAGINATION_VIEWS_PER_PAGE = 6
+
+
+
+@app.route('/getVipCode', methods=['POST'])
+def vipConfig():
+    if request.method == 'POST':
+        codes = ['codility', 'sugarpop', 'regex']
+        coupon_form = request.form['input-code']
+        current_user_info = User.query.filter_by(email=current_user.email).first()
+        for code in codes:
+            if code == coupon_form:
+                current_user_info.vip = True
+                db.session.commit()
+                return jsonify({'res': code})
+        return abort(404);
+
+
+@app.route('/vipmembers')
+@app.route('/vipmembers/<int:page>')
+def vipMembers(page=1):
+    link = User.query.paginate(page, PAGINATION_VIEWS_PER_PAGE, False)
+    return render_template('vip/index.html', link=link)
