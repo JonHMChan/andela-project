e36e5a4448d63b3e8f7a280a26cc7023316c3420
Babajide Fowotade <babajide.fowotade@andela.co>
2015-08-20 12:29:04 +0100

    changed debug to false, double check

--- run.py
+++ run.py
@@ -133,6 +133,7 @@ def progLangRoute():
 
 
 if __name__ == '__main__':
+    app.config['DEBUG'] = False
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(DEBUG=False, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
